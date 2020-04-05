c4e885bbfd8e1ff70a14fa262f5494b05e8224a7
unknown <karolus@naver.com>
2020-04-05 20:17:03 +0900

    test

--- src/main/java/com/test/controller/MainController.java
+++ src/main/java/com/test/controller/MainController.java
@@ -14,26 +14,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+@RequestMapping("/main")
 @Controller
-public class HomeController {
+public class MainController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
+		model.addAttribute("msg", "메인");
 		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "main";
 	}
 	
 }
